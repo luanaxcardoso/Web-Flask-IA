12a69ded84d42093e95a8cd7a94e3cb62a17aebf
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-02-22 18:43:22 -0300

    initial

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, redirect, url_for, request, render_template, session
+import requests, os, uuid, json
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def index_post():
+   
+   # Pega o texto original e o idioma alvo do formulário
+    original_text = request.form['text']
+    target_language = request.form['language']
+
+    # Pega as variáveis de ambiente
+    key = os.environ['KEY']
+    endpoint = os.environ['ENDPOINT']
+    location = os.environ['LOCATION']
+
+    # Constrói a URL
+    path = '/translate?api-version=3.0'
+    target_language_parameter = '&to=' + target_language
+    constructed_url = endpoint + path + target_language_parameter
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    body = [{ 'text': original_text }]
+
+    translator_request = requests.post(constructed_url, headers=headers, json=body)
+    
+    translator_response = translator_request.json()
+    
+    translated_text = translator_response[0]['translations'][0]['text']
+
+    return render_template(
+        'results.html',
+        translated_text=translated_text,
+        original_text=original_text,
+        target_language=target_language
+    )
